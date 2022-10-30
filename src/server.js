30f53900c6dd715630f48bea154db89ca5256f94
kjhto46 <tony1996kjh@gmail.com>
2022-10-30 20:53:38 +0900

    middleware use get

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`위치 이동 하려는 중 : ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
 const handleHome = (req, res) => {
-  return res.send("I love middleware");
+  return res.send("I love middlewares");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
