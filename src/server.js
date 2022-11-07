2f92993026796b6d564d5367f5780189d081d183
kjhto46 <tony1996kjh@gmail.com>
2022-11-07 23:45:01 +0900

    router 초반 간편 정리

--- src/server.js
+++ src/server.js
@@ -1,27 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const glogalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-glogalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-videoRouter.get("/watch", handleWatchVideo);
-
 // 라우터 추가
-app.use("/", glogalRouter);
-app.use("/videos", videoRouter);
+app.use("/", globalRouter);
 app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.use(logger);
 
