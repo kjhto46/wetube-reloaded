bdbe7e2de030e6568ca7d2dfaa78db9760a7b2ee
kjhto46 <tony1996kjh@gmail.com>
2022-11-07 23:23:30 +0900

    routers불러오는 방법

--- src/server.js
+++ src/server.js
@@ -6,16 +6,24 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const home = (req, res) => {
-  return res.send("hello");
-};
-const login = (req, res) => {
-  return res.send("login");
-};
+const glogalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+glogalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+// 라우터 추가
+app.use("/", glogalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
