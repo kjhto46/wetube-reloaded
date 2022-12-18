3ee3b1e9bd9a7cb51f42e58a53775e745af2d0c4
kjhto46 <tony1996kjh@gmail.com>
2022-12-18 22:03:15 +0900

    데이터 불러오기 준비

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,6 +1,4 @@
-export const trending = (req, res) => 
-{
-  const videos = [    
+let videos = [    
     {
       title: "First Video",
       rating: 5,
@@ -15,7 +13,7 @@ export const trending = (req, res) =>
       comments: 2,
       createdAt: "2 minutes ago",
       views: 59,
-      id: 1,
+      id: 2,
     },
     {
       title: "Third Video",
@@ -23,12 +21,18 @@ export const trending = (req, res) =>
       comments: 2,
       createdAt: "2 minutes ago",
       views: 59,
-      id: 1,
+      id: 3,
     },
   ];
+
+export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Uploads");
