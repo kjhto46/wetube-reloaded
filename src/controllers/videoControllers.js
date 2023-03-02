58b3d782516e9c8c5bd46ca4fefc7017e63e4228
kjhto46 <tony1996kjh@gmail.com>
2023-03-02 23:59:12 +0900

    비디오 정보 edit

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -33,10 +33,9 @@ export const watch = (req, res) => {
   const video = videos[id - 1];
   return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Uploads");
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    return res.send("Delete Video")
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", {pageTitle:`Editing: ${video.title}`, video})
 };
+export const postEdit = (req, res) => {}; 
